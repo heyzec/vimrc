8f8a467dcbf0e9107a9c557213df40e8997edcd4
heyzec <61238538+heyzec@users.noreply.github.com>
2024-11-15 14:50:49 +0800

    hahal

--- nix/hosts/homelab/modules/home-assistant.nix
+++ nix/hosts/homelab/modules/home-assistant.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ pkgs, config, ... }:
 {
   services.home-assistant = {
     enable = true;
@@ -7,25 +7,33 @@
       "xiaomi_ble"
       "zha"
       "mqtt"
+      "samsungtv"
     ];
     extraPackages = ps: with ps; [
       getmac
       pyatv
+      samsungctl
+      samsungtvws
+    ];
+    customComponents = [
+      (pkgs.callPackage ../../../packages/home-assistant/custom_components/samsungtv-tizen.nix {})
     ];
 
     # Your configuration.yaml as a Nix attribute set.
     config = {
       default_config = {};
       homeassistant = {
-        internal_url = "https://heyzec.dedyn.io";
-        external_url = "https://heyzec.dedyn.io";
+        internal_url = "https://home.heyzec.dedyn.io";
+        external_url = "https://home.heyzec.dedyn.io";
       };
       http = {
         # Setup for reverse proxy to properly accept connections
         use_x_forwarded_for = true;
         trusted_proxies = [ "127.0.0.1" "::1" ];
       };
+
       automation = "!include automations.yaml";
+      script = "!include scripts.yaml";
     };
   };
 
@@ -45,11 +53,15 @@
     settings = {
       homeassistant = config.services.home-assistant.enable;
       frontend = true;
+      serial = {
+        adapter = "ember";
+      };
     };
   };
 
+  # 8080 is for Zigbee2MQTT frontend
   networking.firewall.allowedTCPPorts = [ 1883 8123 8080 ];
-  services.caddy.virtualHosts."ha.heyzec.dedyn.io".extraConfig = ''
+  services.caddy.virtualHosts."home.heyzec.dedyn.io".extraConfig = ''
     reverse_proxy * localhost:8123
   '';
 }
